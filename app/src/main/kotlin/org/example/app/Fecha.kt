97844f69cb467ba9a67c039804e678da49412c2f
minibautista14-tech <minibautista14@gmail.com>
2026-06-01 08:13:23 -0300

    Clase-1-6-2026

---
+++ app/src/main/kotlin/org/example/app/Fecha.kt
@@ -0,0 +1,7 @@
+package org.example.app
+
+class Fecha (val dia: Int, val mes: Int, val anio: Int){
+    override fun toString(): String{
+        return "Hoy es $dia del mes $mes, del año $anio"
+    }
+}
